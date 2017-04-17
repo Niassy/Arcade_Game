36bb6f422dff93c5607e0fb61427d07ddd29ae86
Niassy <mamadoujbniassy@outlook.com>
2017-04-17 11:57:13 +0000

    Add files via upload

---
+++ Classes/Type_State.h
@@ -0,0 +1,24 @@
+#ifndef _TYPESTATE_
+
+#define _TYPESTATE_
+
+enum type_state
+{
+	type_Leweto,
+	type_MoveForward,
+	type_MoveDownward,
+	type_Punch,
+	type_Uppercut,
+	type_TakePunch,
+	type_Grab,
+	type_Projection,
+	type_BeProjected,
+	type_Defeat,
+	type_Dance,
+	type_Etourdi,
+	type_Block,
+	type_PunchRoffo,
+	type_TakePunchRoffo
+};
+
+#endif
